2a4aeaad63e41fe08fe494faf4fe87907267cec3
posporus <pfuiba@googlemail.com>
2016-11-22 19:36:36 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var config = require('./config.json');
@@ -34,7 +33,6 @@ var app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
